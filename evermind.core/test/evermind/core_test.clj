f0b64176c4da9cdee21d65f821543e315c58b962
David Caspar <david.caspar@aol.ch>
2017-04-29 16:12:33 +0200

    Add lein-modules plugin.

--- evermind.core/test/evermind/core_test.clj
+++ evermind.core/test/evermind/core_test.clj
@@ -1,7 +1,3 @@
 (ns evermind.core-test
   (:require [clojure.test :refer :all]
             [evermind.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- project.clj
+++ project.clj
@@ -4,8 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-modules "0.3.11"]]
+
+  :modules {:versions {org.clojure/clojure "1.9.0-alpha16"}}
 
   :main ^:skip-aot evermind.core
   :target-path "target/%s"
+
   :profiles {:uberjar {:aot :all}})
