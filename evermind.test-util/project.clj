56839ed346bcec8a00eacfc129d72eeee93b63fd
David Caspar <david.caspar@aol.ch>
2017-04-29 17:47:03 +0200

    Add evermind.test-util module.

---
+++ evermind.test-util/project.clj
@@ -0,0 +1,9 @@
+(defproject evermind.test-util "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :plugins [[lein-modules "0.3.11"]]
+
+  :dependencies [[org.clojure/clojure "_"]])
---
+++ evermind.test-util/test/evermind/test_util_test.clj
@@ -0,0 +1,7 @@
+(ns evermind.test-util-test
+  (:require [clojure.test :refer :all]
+            [evermind.test-util :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
