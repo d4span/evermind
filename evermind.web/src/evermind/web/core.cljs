42493370be59670b2ca181af9ad459a536af510a
Matthias Putz <matthias.putz@innoq.com>
2017-05-01 12:33:00 +0200

    Added mind map visualization as simple tree-list.

--- evermind.web/src/evermind/web/core.cljs
+++ evermind.web/src/evermind/web/core.cljs
@@ -10,18 +10,39 @@
 ;; define your app data so that it doesn't get over-written on reload
 
 (def init-mindmap
-  (d/set-attributes (d/create-mindmap) {:text "Root node"}))
+  (d/add-child
+    (d/set-attributes (d/create-mindmap) {:text "Root node"})
+    (d/add-child (d/set-attributes (d/create-node) {:text "Child 1"})
+                 (d/set-attributes (d/create-node) {:text "Child 1-2"}))
+    (d/set-attributes (d/create-node) {:text "Child 2"})
+    (d/set-attributes (d/create-node) {:text "Child 3"})))
 
-(defonce app-state (atom {:text "hello" :mindmap init-mindmap}))
+(defonce app-state (atom
+                     {:text    "hello"
+                      :mindmap init-mindmap}))
 
-(om/root
-  (fn [data owner]
-      (reify om/IRender
-             (render [_]
-                     (dom/div nil
-                              (dom/h1 nil (-> data :mindmap :attributes :text))))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+(defn node-to-string [node]
+      (-> node :attributes :text))
+
+(defn node-view [node owner]
+      (reify
+        om/IRender
+        (render [this]
+                (dom/li nil
+                        (dom/h2 nil (node-to-string node))
+                        (apply dom/ul nil
+                               (om/build-all node-view (:children node)))))))
+
+(defn mindmap-view [data owner]
+      (reify
+        om/IRender
+        (render [this]
+                (dom/div nil
+                         (dom/h2 nil "Mind Map")
+                         (dom/ul nil
+                                 (om/build node-view (:mindmap data)))))))
+
+(om/root mindmap-view app-state {:target (. js/document (getElementById "app"))})
 
 (defn on-js-reload [])
 ;; optionally touch your app-state to force rerendering depending on
